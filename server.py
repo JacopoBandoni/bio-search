7e19fc7ccccf7ad9325b14b6ba808a2704cf4704
Pier Paolo Tarasco <pier.tarasco@gmail.com>
2022-04-27 23:05:25 +0200

    add options api

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 import json
 from pubmad import get_graph
@@ -7,8 +7,15 @@ import networkx as nx
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/get_graph/<string:query>/<int:max_publications>")
-def API_get_graph(query, max_publications):
-    G = get_graph(query, max_publications=max_publications)
+@app.route("/get_graph", methods=['POST'])
+def API_get_graph():
+    data = request.get_json()
+    query = data['query']
+    max_publications = data['max_publications']
+    start_year = data['start_year']
+    end_year = data['end_year']
+    use_biobert = data['use_biobert']
+    use_biobert = bool(int(use_biobert))
+    G = get_graph(query, max_publications=max_publications, start_year=start_year, end_year=end_year, use_biobert=use_biobert)
     cy_data = json.dumps(nx.readwrite.json_graph.cytoscape_data(G))
     return cy_data
