1b25c081eb4643904f789756d81c0ac85a4f28bc
Pier Paolo Tarasco <pier.tarasco@gmail.com>
2022-04-27 16:36:03 +0200

    add get_graph api callback

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 import json
+from pubmad import get_graph
+import networkx as nx
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/get_graph/<string:query>")
-def get_graph(query):
-    return json.dumps({"response": query})
+def API_get_graph(query):
+    G = get_graph(query)
+    cy_data = json.dumps(nx.readwrite.json_graph.cytoscape_data(G))
+    return cy_data
