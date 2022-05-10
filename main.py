53c9195dc95e1385fd092f68fa2b4abad921cc9f
Pier Paolo Tarasco <pier.tarasco@gmail.com>
2022-05-10 11:59:08 +0200

    Add callback_fn on partial graph

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pubmad import get_graph, get_communities, html_graph, display_graph
 
-G = get_graph('diabetes', max_publications=10, use_biobert=True, clear_cache=False)
+G = get_graph('diabetes', max_publications=2, use_biobert=True, clear_cache=True)
 
 communities = get_communities(G, "weight")
 
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from flask_socketio import send, emit, SocketIO
 from flask_cors import CORS
 import json
 from pubmad import get_graph, get_communities
@@ -17,7 +18,7 @@ def API_get_graph():
     use_biobert = data['use_biobert']
     use_biobert = bool(int(use_biobert))
     G = get_graph(query, max_publications=max_publications, start_year=start_year, end_year=end_year, use_biobert=use_biobert,
-        clear_cache=False)
+        clear_cache=False)#, callback_fn=lambda partial_G: emit('update', {'cy_data': json.dumps(nx.readwrite.json_graph.cytoscape_data(partial_G))}))
     communities = get_communities(G, 'weight', seed=42)
     cy_data = json.dumps(nx.readwrite.json_graph.cytoscape_data(G))
     response = {
