dfc224555853134509facaba5931ad363843f635
Pier Paolo Tarasco <pier.tarasco@gmail.com>
2022-05-10 09:36:13 +0200

    merge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from pubmad import get_graph, get_communities, html_graph, display_graph
-import json
-import networkx as nx
 
 G = get_graph('diabetes', max_publications=10, use_biobert=True, clear_cache=False)
 
