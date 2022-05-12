2b950349dc0cc9a5c3f65a0380d33346a5ee0d0e
Pier Paolo Tarasco <pier.tarasco@gmail.com>
2022-05-12 15:50:02 +0200

    add logo

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 from pubmad import *
+G = get_graph('diabetes', 100)
 
-G = get_graph('diabetes', 10)
-
-insulin = filter_by_name(G, 'insulin')[0]
-
-paths = search_paths_to_category(G, insulin, 'drug')
-
-new_G = get_graph_from_paths(G, paths)
-
-display_graph(G, show=False)
-
-display_graph(new_G)
+html_graph(G, 'diabetes.html')
